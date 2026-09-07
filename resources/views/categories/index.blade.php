b331698b612f77377b380cfe0db2cc50787f4d3b
Nurul Annissa Azzahra <aaannisanurul@gmail.com>
2026-09-08 00:35:16 +0700

    implement basic MVC

---
+++ resources/views/categories/index.blade.php
@@ -0,0 +1,2 @@
+<h1>Kategori Buku</h1>
+    <p>Sistem Informasi Perpustakaan</p>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\MemberController;
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/categories', [CategoryController::class, 'index']);
+Route::get('/members', [MemberController::class, 'index']);
+
 Route::get('/', function () {
     return view('welcome');
 });
