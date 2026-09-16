9253b5bbe82a3a5486b69aec715873b379d37fa3
Nurul Annissa Azzahra <aaannisanurul@gmail.com>
2026-09-17 00:46:51 +0700

    edited code

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,36 +1,15 @@
 @extends('layouts.app')
 
-@section('title', 'Daftar Buku')
+@section('title', 'Dashboard')
 
 @section('content')
-    <h2>Daftar Buku</h2>
+    <h2>{{ $title }}</h2>
+    <p>{{ $description }}</p>
 
-    @if($stock > 0)
-        <p><em>Status: Buku tersedia (Stok: {{ $stock }})</em></p>
-    @else
-        <p><em>Status: Buku sedang habis.</em></p>
-    @endif
-
-    <table border="1" cellpadding="8" cellspacing="0">
-        <thead>
-            <tr>
-                <th>No</th>
-                <th>Judul Buku</th>
-                <th>Penulis</th>
-                <th>Tahun Terbit</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($books as $book)
-                <tr>
-                    <td>{{ $book['id'] }}</td>
-                    <td>{{ $book['title'] }}</td>
-                    <td>{{ $book['author'] }}</td>
-                    <td>{{ $book['year'] }}</td>
-                    <td><a href="/books/{{ $book['id'] }}">Detail</a></td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <h3>Statistik Perpustakaan</h3>
+    <ul>
+        <li>Total Buku: {{ $totalBooks }}</li>
+        <li>Total Kategori: {{ $totalCategories }}</li>
+        <li>Total Member: {{ $totalMembers }}</li>
+    </ul>
 @endsection
